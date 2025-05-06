888c06b3dc38daf1199294ead76fe3c92e4ac205
anurag26-q <anuragparashar111@gmail.com>
2025-05-06 18:03:03 +0530

    cahnges in the workflow file and requriment file

---
+++ agent/work_flow.py
@@ -0,0 +1,31 @@
+from langgraph.graph import StateGraph,START
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt.tool_node import ToolNode,tools_condition
+
+from langchain_core.messages import AIMessage,HumanMessage
+from typing_extensions import Annotated,TypedDict
+from utils.model_loader import ModelLoader
+from toolkit.tool import *
+
+
+
+
+
+class State(TypedDict):
+    messages:Annotated[list,add_messages]
+
+
+class GrapgBuilder:
+    def __init__(self):
+        pass
+
+    def _chatbot_node(self,state:State):
+        pass
+
+    def build(self):
+        pass
+
+    def get_graph(self):
+        pass
+
+
