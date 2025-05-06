652c8311c54c2b9a61d0f7b60fe05b2eb3d269cd
anurag26-q <anuragparashar111@gmail.com>
2025-05-06 23:54:42 +0530

    upload api is created

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI,UploadFile,File,Request
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+from starlette.responses import JSONResponse
+from data_ingestion.data_ingestion_pipeline import DataIngestion
+from agent.work_flow import GrapgBuilder
+from data_models.models import * 
+
+
+app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials = True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
+@app.post('/uplpad')
+async def upload_files(files:List[UploadFile]=File(...)):
+    try:
+        ingestion=DataIngestion()
+        ingestion.run_pipeline(files)
+        return {'messages':'Files successfully processed and stored.'}
+    except Exception as e:
+        return JSONResponse(status_code=500,content={'error':str(e)})
